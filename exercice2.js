78fbe2a7b5778171761bf2b12c7c0e63c3939769
Niouk971 <florian.polter.77@gmail.com>
2025-06-03 15:02:01 +0200

    Ajout exercices 2 et 3

---
+++ exercice2.js
@@ -0,0 +1,13 @@
+// exercice2
+
+const firstName = "Florian";
+const lastName = "Polter";
+
+alert(`Bonjour, ${firstName} ${lastName}`)
+
+const number1 = 64
+
+let age = 28
+let total = number1 - age
+
+alert(`${total} années à travailler avant la retraite ! Courage ! En développant ce sera plus fun !`);
