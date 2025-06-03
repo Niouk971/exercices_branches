38f2fb259313fac4e8e7c12332a3bcd9b9dabb58
Niouk971 <florian.polter.77@gmail.com>
2025-06-03 10:27:13 +0200

    Correction exercice debug

--- syntaxe/debug.js
+++ syntaxe/debug.js
@@ -1,27 +1,25 @@
-Panneau latéral
-1
-FKAP-VARIABLES
-Pratiquer
-Exercice 1 : Deboguage
-
-Exercice 1 : Deboguage
-Conditions d’achèvement
-À faire : Remettre un travail
-Corrige le code suivant en supprimant les bugs et en utilisant les bonnes pratiques pour renommer les variables. Note quelque part toutes les erreurs que t’affiche la console et essaie de comprendre comment tu les as corrigées.
-
 // Nous souhaitons faire la somme du prix de plusieurs articles
 // Prix de deux articles
-let a1 = 10
-let a2 = 20
-// Nous calculons le total
-const tot = a1 + a2
-// Prix d'un troisieme article
-let a3 = "50"
-// Nous ajoutons a3 au total
-tot = tot + a3
 
-console.log("Le prix total avant reduction est de : ", tot)
-console.log("Le prix total après reduction est de : ", tot2)
+let priceArticle1 = 10
+let priceArticle2 = 20
+
+// Nous calculons le total
+
+let total = priceArticle1 + priceArticle2
+
+// Prix d’un troisieme article
+
+let priceArticle3 = 50
+
+// Nous ajoutons a3 au total
+
+total += priceArticle3
 
 //Nous appliquons 20% de reduction
-let tot2 = tot - tot*0,2
+
+let reduction = total*0.2
+let finalPrice = total - reduction
+
+console.log("Le prix total avant reduction est de : ", total)
+console.log("Le prix total après reduction est de : ", finalPrice)
