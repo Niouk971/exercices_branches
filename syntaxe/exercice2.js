30f3894423d08f7cb43828aa0e09c54a6d7f8404
Niouk971 <florian.polter.77@gmail.com>
2025-06-04 16:23:45 +0200

    ajout commentaires exemples boucles Vi

--- syntaxe/exercice2.js
+++ syntaxe/exercice2.js
@@ -11,3 +11,19 @@ let age = 28
 let total = number1 - age
 
 alert(`${total} années à travailler avant la retraite ! Courage ! En développant ce sera plus fun !`);
+
+// i++
+// i += 1
+// i = i + 1
+
+// for(let i = 0; i <= 4; i++) {
+//   console.log(i);
+// }
+
+// for(let i = 6; i <= 20; i += 2) {
+//   console.log(i);
+// }
+
+// for(let i = 0; i < promotions.length; i++) {
+//   console.log(promotions[i]);
+// }
