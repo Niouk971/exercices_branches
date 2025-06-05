3e64089f4ee4595dd2228b5d06ae011dab8d8fe8
Niouk971 <florian.polter.77@gmail.com>
2025-06-05 16:00:19 +0200

    Ajout exercices_individuels

---
+++ exercices_individuels/bonjour_javascript/script.js
@@ -0,0 +1,19 @@
+// etape 2
+
+let message = "Bonjour !";
+
+// console.log(message);
+
+let firstName = "Beyonce";
+message = `Bonjour ${firstName} !`;
+
+// console.log(message);
+
+// etape 3
+
+function sayHello(){
+    console.log(message);
+}
+
+sayHello();
+
